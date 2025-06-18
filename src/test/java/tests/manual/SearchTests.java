fc0cfa269938f9605e9dcca60b6d1ee99d283449
YanaTsiFi <yanagrischechko@gmail.com>
2025-06-18 17:11:35 +0300

    added project

---
+++ src/test/java/tests/manual/SearchTests.java
@@ -0,0 +1,29 @@
+package tests.manual;
+
+import annotations.Manual;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class SearchTests {
+
+    @Test
+    @Manual
+    @Disabled("Ручной тест – выполняется вручную через TestOps")
+    @DisplayName("Проверка поиска в профиле")
+    public void shouldSearchCorrectlyInProfile() {
+        /*
+         Предусловие: Пользователь авторизован и находится в разделе "Профиль"
+
+         Шаги:
+         1. Кликнуть на иконку поиска или в строку поиска
+         2. Ввести запрос, например: "настройки"
+         3. Убедиться, что отображаются релевантные результаты
+         4. Кликнуть по одному из результатов
+         5. Убедиться, что открылась соответствующая страница или раздел
+
+         Ожидаемый результат:
+         Система корректно обрабатывает поисковой запрос и отображает релевантные результаты
+         */
+    }
+}
